ce552af1000f5847c4ee4b35afad288fe9934518
Ajay K <76083469+ajaypep@users.noreply.github.com>
2024-02-17 12:15:13 +0530

    feat!: Add GameBoard

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('hello world');
+const GameBoard = (() => {
+    let gameState = [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null],
+    ];
+    const setXO = (setX, row, col) => {
+        gameState[row][col] = setX ? 'X' : 'O';
+    };
+    const reset = () => {
+        gameState = new Array(3).fill(new Array(3).fill(null, 0, 3), 0, 3);
+    };
+    const print = () => {
+        console.log(gameState);
+    };
+
+    return { setXO, reset, print };
+})();
